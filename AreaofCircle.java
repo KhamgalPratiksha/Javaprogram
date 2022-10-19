8ab474fd30dbfb6856257af48dfdb038161671c3
KhamgalPratiksha <116071378+KhamgalPratiksha@users.noreply.github.com>
2022-10-19 11:09:53 +0530

    Add files via upload

---
+++ AreaofCircle.java
@@ -0,0 +1,13 @@
+package com.area;
+
+public class AreaofCircle {
+
+	public static void main(String[] args) {
+	int radius=9;
+	double pi=3.142,area;
+	area=pi*radius*radius;
+	System.out.println("The Area of circle with radius=9 is"+area);
+
+	}
+
+}
