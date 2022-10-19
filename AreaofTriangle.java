7b211763707eb210a08605e4314d0fadf491656b
KhamgalPratiksha <116071378+KhamgalPratiksha@users.noreply.github.com>
2022-10-19 11:16:31 +0530

    Add files via upload

---
+++ AreaofTriangle.java
@@ -0,0 +1,27 @@
+package com.area;
+
+public class AreaofTriangle {
+
+	public static void main(String[] args) {
+	int len;
+	int bre;
+	
+	//Assign values
+	
+	len=6;
+	bre=4;
+	
+	//Finding area
+	
+	int Area=len*bre/2;
+	
+	//Finding the area
+	
+	System.out.println("The area of the triangle size of"+len+"length"+bre+"breadth is"+Area);
+	
+
+	
+
+	}
+
+}
