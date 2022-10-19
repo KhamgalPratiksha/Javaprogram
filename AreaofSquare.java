0ab73869886525bfac9507b3749c2a5428159ced
KhamgalPratiksha <116071378+KhamgalPratiksha@users.noreply.github.com>
2022-10-19 11:13:46 +0530

    Add files via upload

---
+++ AreaofSquare.java
@@ -0,0 +1,22 @@
+package com.area;
+
+public class AreaofSquare {
+
+	public static void main(String[] args) {
+	float side,area;
+	
+	//Assign values
+	
+	side=10;
+	
+	//Finding area
+	
+	float Area=side*side;
+	
+	//Finding the area
+	
+	System.out.println("The area of Square with side=10 is "+Area+"sq unit.");
+
+	}
+
+}
