71d9ba40d4997a54978856af4c0f24dc39aba639
KhamgalPratiksha <116071378+KhamgalPratiksha@users.noreply.github.com>
2022-10-19 11:11:48 +0530

    Add files via upload

---
+++ AreaofRectangle.java
@@ -0,0 +1,23 @@
+package com.area;
+
+public class AreaofRectangle {
+
+	public static void main(String[] args) {
+	int len;
+	int bre;
+	
+	//Assign values
+	
+	len=4;
+	bre=6;
+	
+	//Finding area
+	int Area=len*bre;
+	
+	//Finding the Area
+	System.out.println("The area of the rectanglesize of "+len+"length"+bre+"breadth is"+Area);
+	
+
+	}
+
+}
