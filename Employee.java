e91d15a473bcab9f5faaf03b5fc3d9d800c31538
KhamgalPratiksha <116071378+KhamgalPratiksha@users.noreply.github.com>
2022-10-20 13:56:38 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,41 @@
+package com.edu;
+
+import java.util.Scanner;
+
+public class Employee {
+
+	public static void main(String[] args) {
+	String ename;
+	int eage;
+	float esalary;
+	String edept;
+	double eyearsalary;
+	
+	Scanner sc = new Scanner(System.in);
+	
+	//User input
+	
+	System.out.println("Enter ename");
+	ename = sc.next();
+	System.out.println("Enter eage");
+	eage = sc.nextInt();
+	System.out.println("Enter esalary");
+	esalary = sc.nextInt();
+	System.out.println("Enter edept");
+	edept = sc.next();
+	System.out.println("Enter eyearsalary");
+	eyearsalary = sc.nextInt();
+	
+	//Display
+	
+	System.out.println("Enter the Name:"+ename);
+	System.out.println("Enter the Age:"+eage);
+	System.out.println("Enter the Salary:"+esalary);
+	System.out.println("Enter the Dept:"+edept);
+	System.out.println("Enter the Yearsalary:"+eyearsalary);
+	
+	
+
+	}
+
+}
