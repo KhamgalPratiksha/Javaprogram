51da29cc5ffd31c305be331ec78b9cce572b9691
KhamgalPratiksha <116071378+KhamgalPratiksha@users.noreply.github.com>
2022-10-20 09:43:29 +0530

    Add files via upload

--- AreaofTriangle.java
+++ AreaofTriangle.java
@@ -3,21 +3,21 @@ package com.area;
 public class AreaofTriangle {
 
 	public static void main(String[] args) {
-	int len;
-	int bre;
+	int base;
+	int height;
 	
 	//Assign values
 	
-	len=6;
-	bre=4;
+	base=6;
+	height=4;
 	
 	//Finding area
 	
-	int Area=len*bre/2;
+	int Area=(base*height)/2;
 	
 	//Finding the area
 	
-	System.out.println("The area of the triangle size of"+len+"length"+bre+"breadth is"+Area);
+	System.out.println("The area of the triangle size of"+base+"base"+height+"height is"+Area);
 	
 
 	
