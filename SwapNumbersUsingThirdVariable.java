93fde22ab16b34538c146f3d81270503cba881fd
KhamgalPratiksha <116071378+KhamgalPratiksha@users.noreply.github.com>
2022-10-20 09:52:05 +0530

    Add files via upload

---
+++ SwapNumbersUsingThirdVariable.java
@@ -0,0 +1,19 @@
+package com.area;
+
+public class SwapNumbersUsingThirdVariable {
+
+	public static void main(String[] args) {
+	int a,b,temp;
+	a=20;
+	b=40;
+	System.out.println("Before interchanging a="+a+" and b="+b);
+	
+	temp=a;
+	a=b;
+	b=temp;
+	
+	System.out.println("After swipping a="+a+" and b="+b);
+
+	}
+
+}
