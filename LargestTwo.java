c91cd6f56cdf77b894f6924e235810c1da47bf3b
KhamgalPratiksha <116071378+KhamgalPratiksha@users.noreply.github.com>
2022-10-20 09:50:15 +0530

    Add files via upload

---
+++ LargestTwo.java
@@ -0,0 +1,15 @@
+package com.edu;
+
+public class LargestTwo {
+
+	public static void main(String[] args) {
+	int a,b,largest;
+	a=15;
+	b=7;
+	largest=(a>b)?a:b;
+	System.out.println("The largest of "+a+" and "+b+" is" +largest);
+
+
+	}
+
+}
