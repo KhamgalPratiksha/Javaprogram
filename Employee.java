ff5672387c2af2fd5df6e0d96c0f85cab7daf804
KhamgalPratiksha <116071378+KhamgalPratiksha@users.noreply.github.com>
2022-10-27 17:20:11 +0530

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,39 +1,23 @@
 package com.edu;
 
-import java.util.Scanner;
-
 public class Employee {
+	int b;
+	static int c=10;
+	
+	void display() {
+		int k=0;
+		System.out.println("b="+b);
+		System.out.println("static data c="+c);
+		System.out.println("local variable k="+k);
+	}
 
 	public static void main(String[] args) {
-	String ename;
-	int eage;
-	float esalary;
-	String edept;
-	double eyearsalary;
-	
-	Scanner sc = new Scanner(System.in);
-	
-	//User input
-	
-	System.out.println("Enter ename");
-	ename = sc.next();
-	System.out.println("Enter eage");
-	eage = sc.nextInt();
-	System.out.println("Enter esalary");
-	esalary = sc.nextInt();
-	System.out.println("Enter edept");
-	edept = sc.next();
-	
-	//Display
-	
-	System.out.println("Enter the Name:"+ename);
-	System.out.println("Enter the Age:"+eage);
-	System.out.println("Enter the Salary:"+esalary);
-	System.out.println("Enter the Dept:"+edept);
-	eyearsalary = esalary*12;
-	System.out.println("Calculated one year salary of the employee:"+eyearsalary);
-	
-	
+		int a=10;
+		Employee eob=new Employee();
+		System.out.println("b="+eob.b);
+		System.out.println("static data c="+Employee.c);
+		System.out.println("static data c="+eob.c);
+		System.out.println("static data c="+c);
 
 	}
 
