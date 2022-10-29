a2319565cdd43080f5385d8216f9726c1a42670e
KhamgalPratiksha <116071378+KhamgalPratiksha@users.noreply.github.com>
2022-10-29 17:23:17 +0530

    Add files via upload

---
+++ WhileLoop.java
@@ -0,0 +1,14 @@
+package com.edu;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		int i=1;//initialization
+		while(i<=100) {//condition
+			System.out.println("welcome");
+			i=i+1;//updation
+		}
+
+	}
+
+}
