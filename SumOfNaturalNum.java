475f2e26488e4885739d4e73e81e700490abc685
KhamgalPratiksha <116071378+KhamgalPratiksha@users.noreply.github.com>
2022-10-29 17:20:27 +0530

    Add files via upload

---
+++ SumOfNaturalNum.java
@@ -0,0 +1,17 @@
+package com.edu;
+
+public class SumOfNaturalNum {
+
+	public static void main(String[] args) {
+		int i=1;
+		int sum=0;
+		while(i<=100) {
+			sum=sum+i;
+			i=i+1;
+		}
+		System.out.println("sum="+sum);
+	
+
+	}
+
+}
