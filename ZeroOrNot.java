cba66f3122cc41eed31b2369556e719ff36e5178
KhamgalPratiksha <116071378+KhamgalPratiksha@users.noreply.github.com>
2022-10-29 17:21:29 +0530

    Add files via upload

---
+++ ZeroOrNot.java
@@ -0,0 +1,20 @@
+package com.edu;
+
+public class ZeroOrNot {
+
+	public static void main(String[] args) {
+		
+		int a=0;
+		
+		if(a==0)
+		{
+			System.out.println("a is zero value");
+		}
+		else
+		{
+			System.out.println("a is not a zero value");
+		}
+
+	}
+
+}
