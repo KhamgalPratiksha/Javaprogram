a788e4d946666cac6e375ee635c78610c1f41384
KhamgalPratiksha <116071378+KhamgalPratiksha@users.noreply.github.com>
2022-11-02 13:27:29 +0530

    Add files via upload

---
+++ ForEachLoop.java
@@ -0,0 +1,13 @@
+package com.ForLoop;
+
+public class ForEachLoop {
+
+	public static void main(String[] args) {
+		int arr[]= {11,21,35,9,10,45};
+		for(int i:arr) {
+			System.out.println(i);
+		}
+
+	}
+
+}
