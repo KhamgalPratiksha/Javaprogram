00e369a97b21192d18770487dcb8e6fe8395d2ca
KhamgalPratiksha <116071378+KhamgalPratiksha@users.noreply.github.com>
2022-11-02 13:25:05 +0530

    Add files via upload

---
+++ InfinitiveDoWhileLoop.java
@@ -0,0 +1,14 @@
+package com.do_while;
+
+public class InfinitiveDoWhileLoop {
+
+	public static void main(String[] args) {
+		do
+		{
+			System.out.println("Infinitive Do While Loop");
+		}
+		while(true);
+
+	}
+
+}
