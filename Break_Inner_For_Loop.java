0d9e641dad9d5703a3cab2a6e0675f25dcced439
KhamgalPratiksha <116071378+KhamgalPratiksha@users.noreply.github.com>
2022-11-02 13:26:48 +0530

    Add files via upload

---
+++ Break_Inner_For_Loop.java
@@ -0,0 +1,21 @@
+package com.ForLoop;
+
+public class Break_Inner_For_Loop {
+
+	public static void main(String[] args) {
+		//outer loop
+		for(int i=1;i<=3;i++)
+		{
+			for(int j=1;j<=3;j++)
+			{
+				if(i==3 && j==3)
+				{
+					break;
+				}
+				System.out.println(i+" "+j);
+			}
+				
+		}
+	}
+
+}
