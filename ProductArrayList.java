b13a55afa68a0c2a09ef7dcc066cc251460668b1
KhamgalPratiksha <116071378+KhamgalPratiksha@users.noreply.github.com>
2022-11-10 14:11:43 +0530

    Add files via upload

--- ProductArrayList.java
+++ ProductArrayList.java
@@ -2,49 +2,64 @@ package eduCollection;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Scanner;
 
 class Product{
 	int pid;
 	String pname;
 	float pprice;
-	public Product(int pid, String pname, float pprice) {
-		super();
-		this.pid = pid;
-		this.pname = pname;
-		this.pprice = pprice;
-	}
-	@Override
-	public String toString() {
-		return "Product [pid=" + pid + ", pname=" + pname + ", pprice=" + pprice + "]";
-	}
 	
+  void inputData() {
+	
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter pid");
+	pid=sc.nextInt();
+	System.out.println("enter pname");
+	pname=sc.next();
+	System.out.println("enter pprice");	
+	pprice=sc.nextFloat();
+}
+
+@Override
+public String toString() {
+	return "Product [pid=" + pid + ", pname=" + pname + ", pprice=" + pprice + "]";
+}
+
 }
 
 public class ProductArrayList {
 
 	public static void main(String[] args) {
-		ArrayList<Product> plist=new ArrayList<Product>();
-		Product p1=new Product(1,"amul",160.5f);
-		Product p2=new Product(2,"kissan",250.8f);
-		Product p3=new Product(3,"mdh",300.65f);
-		Product p4=new Product(4,"chitale",500.4f);
 		
+		ArrayList<Product> plist= new ArrayList<Product>();
+		Product p1=new Product();
+		Product p2=new Product();
+		Product p3=new Product();
+		Product p4=new Product();
 		
+		p1.inputData();
+		p2.inputData();
+		p3.inputData();
+		p4.inputData();
+			
 		plist.add(p1);
 		plist.add(p2);
 		plist.add(p3);
 		plist.add(p4);
-		
-		
-				
-		System.out.println(plist);
-		System.out.println("****************************");
 			
+		System.out.println(plist);
+		System.out.println("*****************************");
+		
+		System.out.println("Using iterator");
 		Iterator<Product> pt=plist.iterator();
 		System.out.println("PID\tPNAME\tPPRICE");
 		while(pt.hasNext()) {
 			Product pob=pt.next();
 			System.out.println(pob.pid+"\t"+pob.pname+"\t"+pob.pprice);
+			
 		}
+	
+		
 		}
-	}
+}
+	
